ed4d76afbac8e94b3b67cf28d1ea69482d046a25
Takeiteasybabe <Takeiteasybabe@mail.ru>
2019-04-26 21:59:44 +0000

    First version of models.py

--- api/app/models.py
+++ api/app/models.py
@@ -1,2 +1,68 @@
 from django.db import models
 
+class User(models.Model):
+    UserID = models.AutoField(primary_key=True)
+    Login = models.CharField(max_length=30)
+    Password = models.CharField(max_length=30)
+    RegistrationDate = models.DateField()
+
+class Admin(models.Model):
+    AdminID = models.ForeignKey(User, on_delete=models.CASCADE)
+    Name = models.CharField(max_length=30)
+    Role = models.CharField(max_length=30) ## As long as we dont have roles
+
+class Customer(models.Model):
+    CustomerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    FirstName = models.CharField(max_length=30)
+    LastName = models.CharField(max_length=30)
+    Adress = models.TextField()
+    Residence = models.TextField()
+    CVID = models.PositiveIntegerField()
+    PortfolioID = models.PositiveIntegerField()
+
+class CVID(models.Model):
+   CVID = models.AutoField(primary_key=true)
+   ## Other
+   CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Appointement(models.Model):
+    AppointementID = models.AutoField(primary_key=true)
+    TimeOfAppointement = models.DateTimeField()
+    PlaceOfAppointement = models.TextField()
+    Comment = models.TextField()
+    Status = models.TextField() ## Gonna be something else I suppose
+    CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Portfolio(models.Model):
+    PortfolioID = models.AutoField(primary_key=true)
+    ## Othert
+    CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Goal(models.Model):
+    GoalID = models.AutoField(primary_key=true)
+    Deadline = models.DateField()
+    Text = models.TextField()
+    Status = models.TextField()
+    CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class QuestionTip(models.Model):
+    QuestionID = models.AutoField(primary_key=true)
+    Text = models.TextField()
+    IsAnonymous = models.BinaryField()
+    Status = models.TextField()
+    Time = models.DateTimeField()
+    CustomerID = models.ForeignKey(Customer, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    AnswerID = models.AutoField(primary_key=true)
+    Text = models.TextField()
+    Status = models.TextField()
+    Time = models.DateTimeField()
+    QuestionID = models.ForeignKey(QuestionTip, on_delete=models.CASCADE)
+    AdminID = models.ForeignKey(Admin, on_delete=models.CASCADE)
+
+class Faq(models.Model):
+    QandAID = models.AutoField(primary_key=true)
+    QuestionText = models.TextField()
+    AnswerText = models.TextField()
+
