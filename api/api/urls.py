59aa0f7472ae36401fc2dbca9cae4fcdff3cc1ab
Takeiteasybabe <takeiteasybabe@mail.ru>
2019-05-13 15:00:18 +0300

    Trying to make a url

--- api/api/urls.py
+++ api/api/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from tastypie.api import Api
+from app.resources import *
+
+v1_api = Api(api_name='v1')
+v1_api.register(UserByIdResource())
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^api/', include(v1_api.urls), name='User_by_ID')
 ]
--- api/app/resources.py
+++ api/app/resources.py
@@ -1,2 +1,9 @@
-
 from tastypie.resources import ModelResource
+
+from app.models import *
+
+class UserByIdResource(ModelResource):
+    class Meta:
+        queryset = User.objects.all()
+        resource_name = 'user/'
+        list_allowed_methods = ['get']
