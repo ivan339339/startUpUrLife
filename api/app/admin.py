2b5657e02284f856e59e7dea5a13c8342d625513
Takeiteasybabe <takeiteasybabe@mail.ru>
2019-05-13 14:05:56 +0300

    Added admin.py for correct models display

--- api/app/admin.py
+++ api/app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from app.models import *
 
 # Register your models here.
+@admin.register(User)
+@admin.register(Admin)
+@admin.register(Customer)
+@admin.register(CVID)
+@admin.register(Appointement)
+@admin.register(Portfolio)
+@admin.register(Goal)
+@admin.register(QuestionTip)
+@admin.register(Answer)
+
+class PersonAdmin(admin.ModelAdmin):
+        pass
